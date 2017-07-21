e7d73f685dae819e62de0c9eae70548ee1aa1686
jca78 <jca78@sfu.ca>
2017-07-20 17:36:44 -0700

    separating

---
+++ server/src/events/schema.js
@@ -0,0 +1,36 @@
+import fs from 'fs'
+import { join } from 'path'
+import { createEvent, getEventById, getEventByGeolocation, getEventByTitle } from './store'
+
+export const schema = [fs.readFileSync(join(__dirname, 'schema.graphql'), 'utf-8')]
+
+export const resolvers = {
+  Query: {
+    event: async (root, args) => {
+      const { id } = args
+      const event = await getEventById(id)
+      return event
+    },
+    eventByTitle: async (root, args) => {
+      const { title } = args
+      const event = await getEventByTitle(title)
+      return event
+    },
+  },
+  Mutation: {
+    createEvent: async (root, args) => {
+      const { input } = args
+      const createdEvent = await createEvent(input)
+      return createdEvent
+    },
+  },
+  // updateEvent: async (root, args) => {
+  //   try {
+  //     const { id, input } = args
+  //     const updatedEvent = await updateEvent(id, input)
+  //     return updatedEvent
+  //   } catch (e) {
+  //     throw e
+  //   }
+  // },
+}
--- server/src/schema.js
+++ server/src/schema.js
@@ -4,10 +4,7 @@ import { join } from 'path'
 import { makeExecutableSchema } from 'graphql-tools'
 
 import { schema as usersSchema, resolvers as usersResolvers } from './users/schema'
-import { getUserById, getUserByEmail, createUser } from './users/store'
-// KC >
-import { createEvent, getEventById, getEventByGeolocation, getEventByTitle } from './events/store'
-// KC <
+import { getUserById, getUserByEmail, createUser, updateUser } from './users/store'
 
 const rootSchema = [fs.readFileSync(join(__dirname, 'schema.graphql'), 'utf-8')]
 
@@ -23,30 +20,26 @@ const rootResolvers = {
       const user = await getUserByEmail(email)
       return user
     },
-    event: async (root, args) => {
-      const { id } = args
-      const event = await getEventById(id)
-      return event
-    },
-    eventByTitle: async (root, args) => {
-      const { title } = args
-      const event = await getEventByTitle(title)
-      return event
-    },
-    // address: (root, args) => getAddress(args.id),
-    // addresses: (root, args) => getAllAddresses(args.address, args.service, args.limit),
   },
   Mutation: {
     createUser: async (root, args) => {
       const { input } = args
-      const createdUser = await createUser(input)
-      return createdUser
+      try {
+        const createdUser = await createUser(input)
+        return { user: createdUser }
+      } catch (e) {
+        throw e
+      }
     },
-    createEvent: async (root, args) => {
-      const { input } = args
-      const createdEvent = await createEvent(input)
-      return createdEvent
-    }
+    updateUser: async (root, args) => {
+      try {
+        const { id, input } = args
+        const updatedUser = await updateUser(id, input)
+        return updatedUser
+      } catch (e) {
+        throw e
+      }
+    },
   },
 }
 
